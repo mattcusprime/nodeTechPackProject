99daa82626b4857584931aaf612e58a4df470b61
mattcusprime <englmt1@gmail.com>
2015-07-21 13:51:46 -0400

    Removed a couple of fs.readFile lines.

--- TechPackProject/js/myProjectFunctionsV2Deferred.js
+++ TechPackProject/js/myProjectFunctionsV2Deferred.js
@@ -1220,13 +1220,6 @@ function getValueDisplayFromKey(strkey, objGarmentProduct) {
     return strvalue;
 };
 
-/*
-fs.readFile(image_origial, function (err, original_data) {
-    fs.writeFile('image_orig.jpg', original_data, function (err) { });
-    var base64Image = original_data.toString('base64');
-    var decodedImage = new Buffer(base64Image, 'base64');
-    fs.writeFile('image_decoded.jpg', decodedImage, function (err) { });
-});*/
 
 
 function pdfPage(objForFile) {
@@ -1248,7 +1241,4 @@ function pdfPage(objForFile) {
         console.log('Child Process stdout: ' + stdout);
         console.log('Child Process stderr: ' + stderr);
     });
-
-
-
 };
