87c1b90883480dbc43d7d8d3e39c4d161c5dad66
mattcusprime <englmt1@gmail.com>
2016-04-05 17:07:53 -0400

    added some new pdf make type functions.

--- TechPackProject/js/customPdfer.js
+++ TechPackProject/js/customPdfer.js
@@ -1,3 +1,19 @@
-﻿function pdfSpec() {
-    var docDefinition = { content: 'This is an sample PDF printed with pdfMake' };
+﻿function pdfSpec(productToSpec) {
+    var docDefinition = {
+        content: 
+        JSON.parse(productToSpec)
+        
+        }
+
+
+    // open the PDF in a new window
+    pdfMake.createPdf(docDefinition).open();
+
+    // print the PDF (not working in this version, will be added back in a couple of days)
+    // pdfMake.createPdf(docDefinition).print();
+
+    // download the PDF
+    pdfMake.createPdf(docDefinition).download();
+
+
 };
