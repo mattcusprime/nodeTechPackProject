6b7947923fd5802916117b4315908ee8d218bd13
mattcusprime <englmt1@gmail.com>
2016-05-09 16:29:37 -0400

    Final Reconciliation for the day, need to be more careful on button removal...

--- TechPackProject/js/myReportGeneratorFunctions.js
+++ TechPackProject/js/myReportGeneratorFunctions.js
@@ -499,7 +499,14 @@ function getLogin(arrAttributeValueListArray, objCurrentGarmentProduct, arrRepor
         //strUrlPrefixWithPass = 'https://' + strUser + ':' + strPwd + '@wsflexwebprd1v.res.hbi.net/'
         //strUrlPrefixWithPass = 'https://' + strUser + ':' + strPwd + '@plmqa.hanes.com/'
         //strUrlPrefixWithPass = 'https://wsflexwebprd1v.res.hbi.net/';
-        strUrlPrefixWithPass = 'https://@plmqa.hanes.com/';
+        strUrlPrefixWithPass = 'http://' + strUser + ':' + strPwd + '@wsflexwebprd1v.res.hbi.net/'
+        //strUrlPrefixWithPass = 'https://@plmqa.hanes.com/';
+        var strCurrentEnvironment = window.location.href;
+        if (strCurrentEnvironment.indexOf('plmqa.hanes') != -1) {
+            //arrOfAttributeValueListIds = ['2381876', '102771', '2381693', '17436676', '100575'];
+            //strUrlPrefixWithPass = 'https://plmqa.hanes.com/';
+            strUrlPrefixWithPass = 'https://' + strUser + ':' + strPwd + '@plmqa.hanes.com/';
+        };
         strReportsXmlUrl = strUrlPrefixWithPass + strReportsXmlSuffix;
         //$.get(strReportsXmlUrl, function (data) { }).done(function (data) {
         var strBase64 = btoa(strUser + ":" + strPwd);
